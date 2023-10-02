4bcdc2a3b91019502b4b31e19c43fdbfad39ecb4
molson82 <wvandebelt82@yahoo.com>
2023-10-02 15:42:10 -0400

    all in one file again, testing

--- glambda-kit.go
+++ glambda-kit.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
+	"github.com/aws/aws-cdk-go/awscdkapigatewayv2integrationsalpha/v2"
+	"github.com/aws/aws-cdk-go/awscdklambdagoalpha/v2"
 	"github.com/subosito/gotenv"
 
 	"github.com/aws/aws-cdk-go/awscdkapigatewayv2alpha/v2"
@@ -29,13 +33,96 @@ func NewGlambdaKitStack(scope constructs.Construct, id string, props *GlambdaKit
 	stack := awscdk.NewStack(scope, &id, &sprops)
 
 	// create HTTP API Gateway
-	httpApiGateway := awscdkapigatewayv2alpha.NewHttpApi(stack, jsii.String("glambda-api"), &awscdkapigatewayv2alpha.HttpApiProps{
+	gateway := awscdkapigatewayv2alpha.NewHttpApi(stack, jsii.String("glambda-api"), &awscdkapigatewayv2alpha.HttpApiProps{
 		ApiName: jsii.String("glambda-api"),
 	})
 
-	createHttpApiGateway(httpApiGateway, stack)
+	// add Square Create Booking Lambda
+	gateway.AddRoutes(&awscdkapigatewayv2alpha.AddRoutesOptions{
+		Path:        jsii.String("/create/booking"),
+		Methods:     &[]awscdkapigatewayv2alpha.HttpMethod{awscdkapigatewayv2alpha.HttpMethod_POST},
+		Integration: awscdkapigatewayv2integrationsalpha.NewHttpLambdaIntegration(jsii.String("MyHttpLambdaIntegration"), postCreateBookingHandler(stack), &awscdkapigatewayv2integrationsalpha.HttpLambdaIntegrationProps{}),
+	})
+
+	// add hello route to HTTP API Gateway
+	gateway.AddRoutes(&awscdkapigatewayv2alpha.AddRoutesOptions{
+		Path:        jsii.String("/golang/hello"),
+		Methods:     &[]awscdkapigatewayv2alpha.HttpMethod{awscdkapigatewayv2alpha.HttpMethod_GET},
+		Integration: awscdkapigatewayv2integrationsalpha.NewHttpLambdaIntegration(jsii.String("MyHttpLambdaIntegration"), getGoHelloHandler(stack), &awscdkapigatewayv2integrationsalpha.HttpLambdaIntegrationProps{}),
+	})
+
+	// add NodeJS Andy route to HTTP API Gateway
+	gateway.AddRoutes(&awscdkapigatewayv2alpha.AddRoutesOptions{
+		Path:        jsii.String("/node/hello"),
+		Methods:     &[]awscdkapigatewayv2alpha.HttpMethod{awscdkapigatewayv2alpha.HttpMethod_GET},
+		Integration: awscdkapigatewayv2integrationsalpha.NewHttpLambdaIntegration(jsii.String("MyHttpLambdaIntegration"), getNodeHelloHanlder(stack), &awscdkapigatewayv2integrationsalpha.HttpLambdaIntegrationProps{}),
+	})
+
+	// add Python route to HTTP API Gateway
+	gateway.AddRoutes(&awscdkapigatewayv2alpha.AddRoutesOptions{
+		Path:        jsii.String("/ai/vertex"),
+		Methods:     &[]awscdkapigatewayv2alpha.HttpMethod{awscdkapigatewayv2alpha.HttpMethod_GET},
+		Integration: awscdkapigatewayv2integrationsalpha.NewHttpLambdaIntegration(jsii.String("MyHttpLambdaIntegration"), postGoogleAIVertexHanlder(stack), &awscdkapigatewayv2integrationsalpha.HttpLambdaIntegrationProps{}),
+	})
 
 	return stack
+}
+
+// Hello World function
+func getGoHelloHandler(stack awscdk.Stack) awscdklambdagoalpha.GoFunction {
+	return awscdklambdagoalpha.NewGoFunction(stack, jsii.String("GoHello"), &awscdklambdagoalpha.GoFunctionProps{
+		Runtime: awslambda.Runtime_GO_1_X(),
+		Entry:   jsii.String("./lambda/golang/hello"),
+		Bundling: &awscdklambdagoalpha.BundlingOptions{
+			GoBuildFlags: jsii.Strings(`-ldflags "-s -w"`),
+		},
+	})
+
+}
+
+// Create Square Booking POST lambda
+func postCreateBookingHandler(stack awscdk.Stack) awscdklambdagoalpha.GoFunction {
+	return awscdklambdagoalpha.NewGoFunction(stack, jsii.String("CreateSquareBooking"), &awscdklambdagoalpha.GoFunctionProps{
+		Runtime: awslambda.Runtime_GO_1_X(),
+		Entry:   jsii.String("./lambda/golang/squareApi/createBooking"),
+		Bundling: &awscdklambdagoalpha.BundlingOptions{
+			GoBuildFlags: jsii.Strings(`-ldflags "-s -w"`),
+		},
+		Environment: &map[string]*string{
+			"SQUARE_API_KEY":            jsii.String(os.Getenv("SQUARE_API_KEY")),
+			"SERVICE_VARIATION_VERSION": jsii.String(os.Getenv("SERVICE_VARIATION_VERSION")),
+			"SERVICE_VARIATION_ID":      jsii.String(os.Getenv("SERVICE_VARIATION_ID")),
+			"TEAM_MEMBER_ID":            jsii.String(os.Getenv("TEAM_MEMBER_ID")),
+			"CUSTOMER_ID":               jsii.String(os.Getenv("CUSTOMER_ID")),
+			"LOCATION_ID":               jsii.String(os.Getenv("LOCATION_ID")),
+		},
+	})
+}
+
+// Hello World Nodejs Function
+func getNodeHelloHanlder(stack awscdk.Stack) awslambda.Function {
+	return awslambda.NewFunction(stack, jsii.String("NodeHello"), &awslambda.FunctionProps{
+		Runtime: awslambda.Runtime_NODEJS_18_X(),
+		Handler: jsii.String("index.main"),
+		Code:    awslambda.Code_FromAsset(jsii.String("./lambda/node/hello/"), nil),
+	})
+}
+
+// Python 3 Lambda function
+func postGoogleAIVertexHanlder(stack awscdk.Stack) awslambda.Function {
+	layer := awslambda.NewLayerVersion(stack, jsii.String("MyLayer"), &awslambda.LayerVersionProps{
+		Code: awslambda.Code_FromAsset(jsii.String("./lambda/python/googleAI/vertex/deployment_package.zip"), nil),
+	})
+	return awslambda.NewFunction(stack, jsii.String("GVertextAI"), &awslambda.FunctionProps{
+		Runtime: awslambda.Runtime_PYTHON_3_9(),
+		Handler: jsii.String("index.handler"),
+		Code:    awslambda.Code_FromAsset(jsii.String("./lambda/python/googleAI/vertex"), nil),
+		Layers:  &[]awslambda.ILayerVersion{layer},
+		Environment: &map[string]*string{
+			"VERTEX_JSON": jsii.String(os.Getenv("VERTEX_JSON")),
+		},
+		Timeout: awscdk.Duration_Seconds(jsii.Number(30)),
+	})
 }
 
 func main() {
